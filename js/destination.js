7163308014eed9dbecc0c5b34f8be3627cdc5706
PedroRocs <pedrorochacardoso1908@gmail.com>
2022-03-16 16:43:51 -0300

    logica em js para inserir dados dos planetas

---
+++ js/destination.js
@@ -0,0 +1,34 @@
+const url = "https://pedro2862.github.io/jsonData.github.io/data.json";
+const h2 = document.querySelector(".titulo-descricao-planeta h2");
+const p = document.querySelector(".titulo-descricao-planeta p");
+const distance = document.querySelector("#distance-info");
+const tempo = document.querySelector("#time-distance");
+const img = document.querySelector(".planet img");
+const opcao = document.querySelectorAll(".nome-planet h2");
+
+fetch(url)
+.then((resp)=> resp.json())
+
+.then(function(data){
+    let dados = data.destinations;
+    for(let i = 0; i<opcao.length;i++){
+        opcao[i].addEventListener("click", () =>{
+            h2.innerHTML=dados[i].name;
+            p.innerHTML=dados[i].description;
+            distance.innerHTML=dados[i].distance;
+            tempo.innerHTML=dados[i].travel;
+            img.src=dados[i].images.png;
+        })
+    }
+  
+   
+           
+       }
+   
+)
+
+.catch(function(error) {
+    window.innerHTML="Recarregue a pegina";
+
+
+});
