bebed9fc352e46b6ae7d1e9976705fb171151185
PedroRocs <pedrorochacardoso1908@gmail.com>
2022-03-19 14:11:47 -0300

    criando logica em js para inseir as informaçoes do json na html, algumas modicaçoes no css para o codigo funcionar onde adicionei um after embaixo dos nomes dos planetas para fazer a linha que mostra o planeta que esta na tela, removi algumas classes e adicionai algumas por meio do js

--- js/destination.js
+++ js/destination.js
@@ -1,34 +1,83 @@
 const url = "https://pedro2862.github.io/jsonData.github.io/data.json";
-const h2 = document.querySelector(".titulo-descricao-planeta h2");
-const p = document.querySelector(".titulo-descricao-planeta p");
-const distance = document.querySelector("#distance-info");
-const tempo = document.querySelector("#time-distance");
-const img = document.querySelector(".planet img");
-const opcao = document.querySelectorAll(".nome-planet h2");
+const descricaoPlaneta = document.querySelector(".titulo-descricao-planeta");
+const distanceInfo = document.querySelector("#distance-info");
+const tempoDistance = document.querySelector("#time-distance");
+const image = document.querySelector(".planet");
+const opcao = document.querySelectorAll(".nome-planet h2")
 
 fetch(url)
-.then((resp)=> resp.json())
+    .then((resp) => resp.json())
+    .then(function (data) {
+        let dados = data.destinations;
+        let contadoraAnterior;
+        for (let i = 0; i < opcao.length; i++) {
 
-.then(function(data){
-    let dados = data.destinations;
-    for(let i = 0; i<opcao.length;i++){
-        opcao[i].addEventListener("click", () =>{
-            h2.innerHTML=dados[i].name;
-            p.innerHTML=dados[i].description;
-            distance.innerHTML=dados[i].distance;
-            tempo.innerHTML=dados[i].travel;
-            img.src=dados[i].images.png;
-        });
-    }
-  
-   
-           
-    }
-   
-)
+            function createElement(element) {
 
-.catch(function(error) {
-    window.innerHTML="Recarregue a pegina";
+                if (element == "h2") {
+                    descricaoPlaneta.innerHTML = "";
+                    let h2 = document.createElement(element);
+                    h2.textContent = dados[i].name;
+                    return h2
+                }
 
+                if (element == "p") {
 
-});
+                    let p = document.createElement(element);
+                    p.textContent = dados[i].description;
+                    return p;
+
+                }
+
+                if (element == "spanTemp") {
+                    document.querySelector(".info-add").remove()
+                    let span = document.createElement("span");
+                    span.classList.add("info-add");
+              
+                    span.textContent=dados[i].travel;
+                    return span;
+                    }
+                if (element == "spanDistance") {
+                    document.querySelector(".info-add").remove()
+                        let span = document.createElement("span");
+                        span.classList.add("info-add")
+                        span.textContent=dados[i].distance;
+                        console.log(span);
+                        return span;
+                        
+                        }
+
+                if (element == "div") {
+                    let div = document.createElement(element);
+                    return div
+                }
+
+                if (element == "img") {
+                    image.innerHTML = "";
+                    let img = document.createElement(element);
+                    img.src = dados[i].images.png;
+                    return img
+                }
+
+            }
+
+            opcao[i].addEventListener("click", () => {
+                if (opcao[0].classList == "after" && i == 1) {
+                    opcao[0].classList.remove("after");
+                }
+                descricaoPlaneta.appendChild(createElement("h2"));
+                descricaoPlaneta.appendChild(createElement("p"));
+                image.appendChild(createElement("img"));
+                tempoDistance.appendChild(createElement("spanTemp"));
+                distanceInfo.appendChild(createElement("spanDistance"));
+                opcao[i].classList.add("after");
+                if (contadoraAnterior != undefined) {
+                    opcao[contadoraAnterior].classList.remove("after");
+                }
+                contadoraAnterior = i;
+            });
+        }})
+
+    .catch(function (error) {
+        window.innerHTML = "Recarregue a pegina";
+    });
